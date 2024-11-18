71c82cb2b82e92b717e3bd4ed5ec053aa5579340
alexey <alexeyweb11@gmail.com>
2024-11-18 22:41:46 +0200

    Initial Commit

---
+++ diabetanduglevosd22/diabetanduglevosd22App.swift
@@ -0,0 +1,17 @@
+//
+//  diabetanduglevosd22App.swift
+//  diabetanduglevosd22
+//
+//  Created by alexey on 18.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct diabetanduglevosd22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
